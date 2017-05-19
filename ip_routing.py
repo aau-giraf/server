4ecf833a3f9757c9ac91ca669fff6da22519dc81
Niclas Moeslund Overby <noverby@prozum.dk>
2017-05-19 14:16:38 +0200

    Add comments

--- ip_routing.py
+++ ip_routing.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python2
 from subprocess import call
-
 ip_addresses = [
+    # Træfik node
     ("192.38.56.37", "172.19.0.233"),
+    # Backup (Temporary)
     ("130.225.198.207", "172.19.0.235"),
+    # Git (Temporary)
     ("192.38.56.36", "172.19.0.232"),
+    # Web (Temporary)
     ("192.38.56.38", "172.19.0.234")
 ]
-
-for (from, to) in ip_addresses:
-    call(["sudo", "iptables", "-t", "nat", "-I", "PREROUTING", "-d", from, "-j", "DNAT", "--to-destination", to])
-    call(["sudo", "iptables", "-t", "nat", "-I", "OUTPUT", "-d", from, "-j", "DNAT", "--to-destination", to])
-    call(["sudo", "iptables-save"])
+for (src, dest) in ip_addresses:
+    call(["sudo", "iptables", "-t", "nat", "-I", "PREROUTING", "-d", src, "-j", "DNAT", "--to-destination", dest])
+    call(["sudo", "iptables", "-t", "nat", "-I", "OUTPUT", "-d", src, "-j", "DNAT", "--to-destination", dest])
+call(["sudo", "iptables-save"])
