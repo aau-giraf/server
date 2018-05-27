d00d5e930603e2e48c99b54c2a052e553b66e02d
Jakob Bo Søndergaard Madsen <jbsm15@student.aau.dk>
2018-05-27 12:04:10 +0000

    Update to reflect new servers

--- ip_routing.py
+++ ip_routing.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python2
 from subprocess import call
 ip_addresses = [
-    # Træfik node
-    ("192.38.56.37", "172.19.0.233"),
-    # Backup (Temporary)
+    # Backup
     ("130.225.198.207", "172.19.0.235"),
-    # Git (Temporary)
+    # Node03 (AKA git atm, remove public IP in future)
     ("192.38.56.36", "172.19.0.232"),
-    # Web (Temporary)
+    # Web
     ("192.38.56.38", "172.19.0.234")
+    # Master node
+    ("192.38.56.136", "172.19.0.243")
 ]
 for (src, dest) in ip_addresses:
     call(["sudo", "iptables", "-t", "nat", "-I", "PREROUTING", "-d", src, "-j", "DNAT", "--to-destination", dest])
     call(["sudo", "iptables", "-t", "nat", "-I", "OUTPUT", "-d", src, "-j", "DNAT", "--to-destination", dest])
-call(["sudo", "iptables-save"])
+
+# This only dumps what iptables looks like right now, use 'iptables save'. 
+# call(["sudo", "iptables-save"])
